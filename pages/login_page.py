1806a9205c903579d563fdd221d48e07261f459e
Paul <paul.mshtk.707@gmail.com>
2023-03-01 19:58:44 +0200

    udate login_page, add user register and setup test data methods

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,6 @@
+import time
+
+import pytest
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
@@ -15,3 +18,23 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not visible"
+
+    def register_new_user(self, email, password):
+        email_input = self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL)
+        email_input.send_keys(email)
+        password_input = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD)
+        password_input.send_keys(password)
+        confirm_password_input = self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD)
+        confirm_password_input.send_keys(password)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        register_button.click()   
+        
+    @pytest.fixture(scope="function")
+    def setup(browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        email = str(time.time()) + "@example.com"  # generate a unique email address for each test run
+        password = "test1234"
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        login_page.register_new_user(email, password)
+        login_page.should_be_authorized_user()     
