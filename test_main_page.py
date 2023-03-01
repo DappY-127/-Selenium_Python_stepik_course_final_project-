8243d94b6d5fd84ed1d5d1145e1358dac020f0d9
Paul <paul.mshtk.707@gmail.com>
2023-03-01 13:10:10 +0200

    add new tests for test_main_page and test_product_page test files

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from selenium.webdriver.common.by import By
@@ -16,4 +17,11 @@ def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()    
+    page.should_be_login_link()  
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    main_page = MainPage(browser, "http://selenium1py.pythonanywhere.com/")
+    main_page.open()
+    main_page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty()      
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from pages.base_page import BasePage
+from pages.basket_page import BasketPage
 from pages.product_page import ProductPage
 from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
@@ -66,4 +67,11 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     product_page.open()
     product_page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()    
+    login_page.should_be_login_page() 
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    product_page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/")
+    product_page.open()
+    product_page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty()       
