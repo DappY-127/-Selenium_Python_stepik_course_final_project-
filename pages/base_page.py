77c2c8e0c83db1f505bf98e0047ee0fbecdb0ce1
Paul <paul.mshtk.707@gmail.com>
2023-03-02 12:39:20 +0200

    update base_page, code rearrangement

--- pages/base_page.py
+++ pages/base_page.py
@@ -13,6 +13,23 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
+    def open(self):
+        self.browser.get(self.url)
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")  
+
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
@@ -33,24 +50,7 @@ class BasePage():
                 until_not(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
-        return True
-    
-    def open(self):
-        self.browser.get(self.url)
-
-    def solve_quiz_and_get_code(self):
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")    
+        return True 
 
     def go_to_login_page(self):
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
