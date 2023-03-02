af8dd4e7bd18531ef81b0ec96bd78c43870c773a
Paul <paul.mshtk.707@gmail.com>
2023-03-02 12:40:49 +0200

    update product_page, code rearrangement

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,6 +6,14 @@ class ProductPage(BasePage):
         cart_button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
         cart_button.click()
 
+    def get_product_name(self):
+        product_name_element = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        return product_name_element.text
+    
+    def get_product_price(self):
+        product_price_element = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        return product_price_element.text    
+
     def should_be_confirmation_add_to_cart_message(self):
         assert self.is_element_present(*ProductPageLocators.CONFIRMATION_FIELD), "'{product} has been added to your basket.' is not visible "   
 
@@ -16,14 +24,6 @@ class ProductPage(BasePage):
     def should_be_cart_total_with_product_price(self, product_price):
         cart_total_message = self.browser.find_element(*ProductPageLocators.PRICE_IN_CONFIRMATION_FIELD).text
         assert product_price == cart_total_message, f"Cart total does not match product price '{product_price}'"
-
-    def get_product_name(self):
-        product_name_element = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
-        return product_name_element.text
-    
-    def get_product_price(self):
-        product_price_element = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
-        return product_price_element.text
     
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
